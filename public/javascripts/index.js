88b1e72b7d9021c163d0155e24e7a8acab4729e2
damir <yespeace@bk.ru>
2020-05-22 20:30:39 +0600

    fx

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,13 @@
 var socket = io('http://161.35.61.122:3000');
 
 socket.on('message', function (data) {
+    location.reload();
     var div = document.getElementById("news-list");
     console.log("Rendering news : ",data);
 
-    for(var i = 0;i < data.length;i++){
-        var newsItem = data[i];
-        div.innerHTML += "<h3>" + newsItem.title + ' <small>'+ newsItem.date + "</small></h3><br>";
-    }
+
+        div.innerHTML += data
+
 
     socket.emit('my other event', { my: 'data' });
 });
