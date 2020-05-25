da8496905f3d160f203aa29cb5e109e30631fb99
damir <yespeace@bk.ru>
2020-05-25 13:14:30 +0600

    fx

--- app.js
+++ app.js
@@ -12,98 +12,10 @@ var usersRouter = require('./routes/users');
 import {postWebHook, sendMessage} from './functions/httpRequest'
 
 var app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 
-var debug = require('debug')('chat:server');
-
-var socketIO = require( "socket.io");
-
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-
-const io = socketIO(server);
-
-
-
-
-
-
-
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-      ? 'Pipe ' + port
-      : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-      ? 'pipe ' + addr
-      : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
 
 
 // view engine setup
@@ -130,7 +42,9 @@ app.post("/", (req, res) => {
     // Send news on the socket
     socket.emit('message', req.body.text);
 
-
+    socket.on('my other event', function (data) {
+      console.log(data);
+    })
   });
 
 
@@ -164,8 +78,7 @@ app.use(function(err, req, res, next) {
 });
 
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
 
 
+
+module.exports = app
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,8 @@
 var socket = io('http://161.35.61.122:3000');
 
 socket.on('message', function (data) {
-
     var div = document.getElementById("news-list");
     console.log("Rendering news : ",data);
-
-
-        div.innerHTML += data
-
-
+    div.innerHTML += data
 
 });
