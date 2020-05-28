a78f01461166f73110428500c916bf5c22770f48
damir <yespeace@bk.ru>
2020-05-29 00:01:49 +0600

    fx

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //
 // import http from 'http'
 // var createError = require('http-errors');
-// var express = require('express');
+var express = require('express');
 // var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-// var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser')
 //
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -108,15 +108,7 @@ import {postWebHook, sendMessage} from './functions/httpRequest'
 //
 //
 // // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-//
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(bodyParser.urlencoded({ extended: false }))
+
 //
 //
 // app.use('/', indexRouter);
@@ -182,6 +174,15 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
 app.get('/', function(req, res){
@@ -197,6 +198,8 @@ app.post("/", (req, res) => {
 
 
 })
+
+
 
 postWebHook()
 
