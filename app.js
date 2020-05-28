d77aa124ebe433409f2bef3d4bf4aa6e0dc0e19e
damir <yespeace@bk.ru>
2020-05-29 03:01:29 +0600

    fx

--- app.js
+++ app.js
@@ -1,178 +1,15 @@
-//
-// import http from 'http'
-// var createError = require('http-errors');
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser')
-//
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-import {postWebHook, sendMessage} from './functions/httpRequest'
-//
-// var app = express();
-//
-//
-//
-// var debug = require('debug')('chat:server');
-//
-// var socketIO = require( "socket.io");
-//
-// /**
-//  * Get port from environment and store in Express.
-//  */
-//
-// var port = normalizePort(process.env.PORT || '3000');
-// app.set('port', port);
-//
-// /**
-//  * Create HTTP server.
-//  */
-//
-// var server = http.createServer(app);
-//
-// /**
-//  * Listen on provided port, on all network interfaces.
-//  */
-//
-//
-// const io = socketIO(server);
-//
-//
-//
-//
-//
-//
-//
-//
-// /**
-//  * Normalize a port into a number, string, or false.
-//  */
-//
-// function normalizePort(val) {
-//   var port = parseInt(val, 10);
-//
-//   if (isNaN(port)) {
-//     // named pipe
-//     return val;
-//   }
-//
-//   if (port >= 0) {
-//     // port number
-//     return port;
-//   }
-//
-//   return false;
-// }
-//
-// /**
-//  * Event listener for HTTP server "error" event.
-//  */
-//
-// function onError(error) {
-//   if (error.syscall !== 'listen') {
-//     throw error;
-//   }
-//
-//   var bind = typeof port === 'string'
-//       ? 'Pipe ' + port
-//       : 'Port ' + port;
-//
-//   // handle specific listen errors with friendly messages
-//   switch (error.code) {
-//     case 'EACCES':
-//       console.error(bind + ' requires elevated privileges');
-//       process.exit(1);
-//       break;
-//     case 'EADDRINUSE':
-//       console.error(bind + ' is already in use');
-//       process.exit(1);
-//       break;
-//     default:
-//       throw error;
-//   }
-// }
-//
-// /**
-//  * Event listener for HTTP server "listening" event.
-//  */
-//
-// function onListening() {
-//   var addr = server.address();
-//   var bind = typeof addr === 'string'
-//       ? 'pipe ' + addr
-//       : 'port ' + addr.port;
-//   debug('Listening on ' + bind);
-// }
-//
-//
-// // view engine setup
 
-//
-//
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-//
-
-//
-//
-//
-//
-// io.on('connection', function(socket){
-//   socket.on('message', function(msg){
-//     console.log('message: ' + msg);
-//   });
-// });
-//
-//
-//
-// io.sockets.on('connection', function (socket) {
-//   socket.emit('message', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
-//
-//
-//
-// postWebHook()
-//
-// // Expose the node_modules folder as static resources (to access socket.io.js in the browser)
-// app.use('/public', express.static(__dirname + '/node_modules/'));
-// app.use('/public', express.static(__dirname + '/public/'));
-//
-//
-//
-// // postWebHook();
-//
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-//
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-//
-//
-// server.listen(port);
-// server.on('error', onError);
-// server.on('listening', onListening);
-//
-//
+import {postWebHook} from './functions/httpRequest'
 
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -182,55 +19,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }))
-
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-let interval;
+app.use(bodyParser.urlencoded({ extended: false }));
 
 io.on("connection", (socket) => {
   console.log("New client connected");
-
-
-
   socket.on("disconnect", () => {
     console.log("Client disconnected");
-
   });
 });
-
 
 app.post("/", (req, res) => {
   console.log(req.body.text)
   console.log(req.body)
   io.sockets.emit('FromAPI', req.body)
-
   res.send(200);
+});
 
-
-})
-
-
-
-
-// const getApiAndEmit = socket => {
-//
-//   // Emitting a new message. Will be consumed by the client
-//   socket.emit("FromAPI", req.body);
-// };
-
-
-
-
-postWebHook()
-
-
-
-
-
-
+postWebHook();
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
