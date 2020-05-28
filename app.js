1ef1fcb9f9f3942749c0e0c399114febdc6dd5ff
damir <yespeace@bk.ru>
2020-05-29 00:02:32 +0600

    fx

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // import http from 'http'
 // var createError = require('http-errors');
 var express = require('express');
-// var path = require('path');
+var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser')
