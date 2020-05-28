d18267e200d4e4801903b1d9893070f4c9da143a
damir <yespeace@bk.ru>
2020-05-28 20:36:49 +0600

    fx

--- app.js
+++ app.js
@@ -1,185 +1,227 @@
-
-import http from 'http'
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require('body-parser')
-
+//
+// import http from 'http'
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// var bodyParser = require('body-parser')
+//
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-import {postWebHook, sendMessage} from './functions/httpRequest'
+// import {postWebHook, sendMessage} from './functions/httpRequest'
+//
+// var app = express();
+//
+//
+//
+// var debug = require('debug')('chat:server');
+//
+// var socketIO = require( "socket.io");
+//
+// /**
+//  * Get port from environment and store in Express.
+//  */
+//
+// var port = normalizePort(process.env.PORT || '3000');
+// app.set('port', port);
+//
+// /**
+//  * Create HTTP server.
+//  */
+//
+// var server = http.createServer(app);
+//
+// /**
+//  * Listen on provided port, on all network interfaces.
+//  */
+//
+//
+// const io = socketIO(server);
+//
+//
+//
+//
+//
+//
+//
+//
+// /**
+//  * Normalize a port into a number, string, or false.
+//  */
+//
+// function normalizePort(val) {
+//   var port = parseInt(val, 10);
+//
+//   if (isNaN(port)) {
+//     // named pipe
+//     return val;
+//   }
+//
+//   if (port >= 0) {
+//     // port number
+//     return port;
+//   }
+//
+//   return false;
+// }
+//
+// /**
+//  * Event listener for HTTP server "error" event.
+//  */
+//
+// function onError(error) {
+//   if (error.syscall !== 'listen') {
+//     throw error;
+//   }
+//
+//   var bind = typeof port === 'string'
+//       ? 'Pipe ' + port
+//       : 'Port ' + port;
+//
+//   // handle specific listen errors with friendly messages
+//   switch (error.code) {
+//     case 'EACCES':
+//       console.error(bind + ' requires elevated privileges');
+//       process.exit(1);
+//       break;
+//     case 'EADDRINUSE':
+//       console.error(bind + ' is already in use');
+//       process.exit(1);
+//       break;
+//     default:
+//       throw error;
+//   }
+// }
+//
+// /**
+//  * Event listener for HTTP server "listening" event.
+//  */
+//
+// function onListening() {
+//   var addr = server.address();
+//   var bind = typeof addr === 'string'
+//       ? 'pipe ' + addr
+//       : 'port ' + addr.port;
+//   debug('Listening on ' + bind);
+// }
+//
+//
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+//
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(bodyParser.urlencoded({ extended: false }))
+//
+//
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+//
+// app.post("/", (req, res) => {
+//   console.log(req.body.text)
+//   console.log(req.body)
+//     io.sockets.emit("message", req.body.text);
+//     res.send(200);
+//
+//
+// })
+//
+//
+//
+//
+// io.on('connection', function(socket){
+//   socket.on('message', function(msg){
+//     console.log('message: ' + msg);
+//   });
+// });
+//
+//
+//
+// io.sockets.on('connection', function (socket) {
+//   socket.emit('message', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
+//
+//
+//
+// postWebHook()
+//
+// // Expose the node_modules folder as static resources (to access socket.io.js in the browser)
+// app.use('/public', express.static(__dirname + '/node_modules/'));
+// app.use('/public', express.static(__dirname + '/public/'));
+//
+//
+//
+// // postWebHook();
+//
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+//
+//
+// server.listen(port);
+// server.on('error', onError);
+// server.on('listening', onListening);
+//
+//
 
+var express = require('express');
+var path = require('path');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-
-
-var debug = require('debug')('chat:server');
-
-var socketIO = require( "socket.io");
-
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-
-const io = socketIO(server);
-
-
-
-
-
-
-
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-      ? 'Pipe ' + port
-      : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-      ? 'pipe ' + addr
-      : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
-
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }))
 
 
+app.use(express.static(__dirname + '/node_modules'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.post("/", (req, res) => {
-  console.log(req.body.text)
-  console.log(req.body)
-    io.sockets.emit("message", req.body.text);
-    res.send(200);
 
-
-})
-
-
-
-
-io.on('connection', function(socket){
-  socket.on('message', function(msg){
-    console.log('message: ' + msg);
-  });
+app.get('/', function(req, res,next) {
+  res.sendFile(__dirname + '/index.html');
 });
 
+server.listen(3000);
 
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('message', { hello: 'world' });
-  socket.on('my other event', function (data) {
+io.on('connection', function(client) {
+  console.log('Client connected...');
+
+  client.on('join', function(data) {
     console.log(data);
   });
+
+  client.on('messages', function(data) {
+    client.emit('broad', data);
+    client.broadcast.emit('broad',data);
+  });
+
 });
-
-
-
-postWebHook()
-
-// Expose the node_modules folder as static resources (to access socket.io.js in the browser)
-app.use('/public', express.static(__dirname + '/node_modules/'));
-app.use('/public', express.static(__dirname + '/public/'));
-
-
-
-// postWebHook();
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-
