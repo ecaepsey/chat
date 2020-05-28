5d83ca675da3780a90747b192bccb4e3853dce18
damir <yespeace@bk.ru>
2020-05-28 20:16:19 +0600

    f

--- app.js
+++ app.js
@@ -134,6 +134,24 @@ app.post("/", (req, res) => {
 
 
 
+let
+    sequenceNumberByClient = new Map();
+
+// event fired every time a new client connects:
+io.on("connection", (socket) => {
+  console.info(`Client connected [id=${socket.id}]`);
+  // initialize this client's sequence number
+  sequenceNumberByClient.set(socket, 1);
+
+  // when socket disconnects, remove it from the list:
+  socket.on("disconnect", () => {
+    sequenceNumberByClient.delete(socket);
+    console.info(`Client gone [id=${socket.id}]`);
+  });
+});
+
+
+
 
 postWebHook()
 
